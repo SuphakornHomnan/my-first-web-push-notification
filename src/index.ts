e36dd95a4ec7e909cf219c57d4f3291c1ed400f7
MrNice <supakorn.h@wdepic.com>
2022-03-02 14:37:21 +0700

    add cors on index file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import notificationRoute from "./notification/controller/web.push.controller";
 import webpushConfig from "./notification/config/webpush.config";
+import cors from "cors";
 
 async function bootstrap() {
   const app = express();
   app.use(express.json());
   webpushConfig();
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
   app.use("/notification", notificationRoute);
   app.get("/", (req, res) => {
     res.send("Hello notification");
