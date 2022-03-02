ba34565b5c62aaaa02efbaa64946e2613c17d35e
MrNice <supakorn.h@wdepic.com>
2022-03-02 14:29:48 +0700

    enable serverless cors

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
 import express from "express";
-import cors from "cors";
 import notificationRoute from "./notification/controller/web.push.controller";
 import webpushConfig from "./notification/config/webpush.config";
+import { enableServerlessCors } from "./allow.cors.serverless";
 
 async function bootstrap() {
   const app = express();
   app.use(express.json());
-  app.use(
-    cors({
-      origin: "*",
-      methods: "GET,POST,PATCH,PUT,DELETE,OPTIONS",
-      credentials: true,
-      optionsSuccessStatus: 200,
-    })
-  );
+  app.use(enableServerlessCors);
   webpushConfig();
   app.use("/notification", notificationRoute);
   app.get("/", (req, res) => {
